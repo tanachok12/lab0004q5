77b05741ddef7134a3a48774db7a22cbc75b2829
tanachok12 <tanachok_d@elearning.ac.th>
2022-08-04 01:57:52 +0700

    q5

---
+++ src/router/index.js
@@ -0,0 +1,92 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import EventListView from '../views/EventListView.vue'
+import AboutView from '../views/AboutView.vue'
+import EventDetails from '@/views/event/EventDetailView.vue'
+import EventRegister from '@/views/event/EventRegister.vue'
+import EventEdit from '@/views/event/EditView.vue'
+import Eventlayout from '@/views/event/EventLayoutView.vue'
+import NotFoundView from '@/views/NotFoundView.vue'
+import NetWorkError from '@/views/NetworkErrorView.vue'
+import EventAirlineDetail from '@/views/event/EventAirlineDetail.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'EventList',
+    component: EventListView,
+    props: (route) => ({
+      page: parseInt(route.query.page) || 1,
+      morepage: parseInt(route.query.morepage) || 5
+    })
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: AboutView
+  },
+  {
+    path: '/event/:id',
+    name: 'EventLayout',
+    props: true,
+    component: Eventlayout,
+    children: [
+      {
+        path: '',
+        name: 'EventDetails',
+        component: EventDetails
+      },
+      {
+        path: 'airlineDetails',
+        name: 'EventAirlineDetail',
+        component: EventAirlineDetail
+      },
+      {
+        path: 'register',
+        name: 'EventRegister',
+        props: true,
+        component: EventRegister
+      },
+      {
+        path: 'edit',
+        name: 'EventEdit',
+        props: true,
+        component: EventEdit
+      }
+    ]
+  },
+  {
+    path: '/404/:resource',
+    name: '404Resource',
+    component: NotFoundView,
+    props: true
+  },
+  {
+    path: '/:catchAll(.*)',
+    name: 'NotFound',
+    component: NotFoundView
+  },
+  {
+    path: '/network-error',
+    name: 'NetworkError',
+    component: NetWorkError
+  },
+  {
+    path: '/event/:id/register',
+    name: 'EventRegister',
+    props: true,
+    component: EventRegister
+  },
+  {
+    path: '/event/:id/edit',
+    name: 'EventEdit',
+    props: true,
+    component: EventEdit
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
---
+++ src/services/EventService.js
@@ -0,0 +1,23 @@
+import axios from 'axios'
+
+const apiClient = axios.create({
+  baseURL: 'https://my-json-server.typicode.com/se331-2022/passengerdb',
+  withCredentials: false,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json'
+  }
+})
+export default {
+  getEvents(perPage, page) {
+    return apiClient.get('/passenger?_page=' + page + '&_limit=' + perPage)
+  },
+  //Added new call
+  getEvent(id) {
+    return apiClient.get('/passenger/' + id)
+  },
+  //Added new call
+  getEventAir(id) {
+    return apiClient.get('/airline/' + id)
+  }
+}
